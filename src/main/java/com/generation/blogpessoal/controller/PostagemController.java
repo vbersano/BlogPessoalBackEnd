815a7152abc9288d0747d9bfc8a82093f6cbe291
Vinicius Bersano <tk3.bersano@hotmail.com>
2022-05-30 12:15:58 -0300

    completando o crud + correcoes

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,10 +2,18 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +26,72 @@ import com.generation.blogpessoal.repository.PostagemRepository;
 public class PostagemController {
 	
 	@Autowired
+	//Autowired faz a injeção de dependencia automaticamente
+	
 	private PostagemRepository postagemRepository;
 	
-	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll() {
-		return ResponseEntity.ok(postagemRepository.findAll());
+	//função Lambda
+	//@PathVariable -> Identificar os parametros na URL // somente para requests
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
 		
-		// select * from tb_postagens;
+		/*optional é uma classe que faz o encapsulamento
+		 *  de um objeto quando esse obj nao pode ser nulo
+		 *  evita o null pointer exception*/
+		
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+		
+		// select * from tb_postagens where id = input;
 	}
+	
+	@GetMapping ("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getAll(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		
+		// select * from tb_postagens where titulo like "%titulo%";
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem (@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		
+	}
+	
+//	@PutMapping
+//	public ResponseEntity<Postagem> putPostagem (@Valid @RequestBody Postagem postagem) {
+//		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+//
+//	}
+	
+	@PutMapping
+	public ResponseEntity<?> putPostagem (@Valid @RequestBody Postagem postagem) { 
+		return postagemRepository.findById(postagem.getId())
+				
+				.map(resposta -> {
+					postagemRepository.save(postagem);
+					return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+				})
+				.orElse(ResponseEntity.notFound().build());	
+	}
+
+	
+//	@DeleteMapping("/{id}")
+//	public void delete(@PathVariable long id) {
+//		postagemRepository.deleteById(id);
+//	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem (@PathVariable Long id) { 
+		return postagemRepository.findById(id)
+				
+				.map(resposta -> {
+					postagemRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());	
+	}
+	
 	
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -9,5 +12,7 @@ import com.generation.blogpessoal.model.Postagem;
 //<classe, parametro do atributo/coluna>
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
 	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 	
+	//select * from tb_postagens where titulo like "%titulo%";
 }
